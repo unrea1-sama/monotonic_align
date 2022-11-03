3d338187564802147702d64b6d79681590792532
Mushan <55142895+mushanshanshan@users.noreply.github.com>
2022-11-03 17:11:00 +1100

    Delete scripts directory

--- scripts/local_install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pip uninstall mushan -y
-
-python setup.py clean
-python setup.py install
--- scripts/upload_pypi.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf dist/*
-python setup.py clean
-python setup.py sdist bdist_wheel
-
-pip install twine
-twine upload --skip-existing dist/* --verbose
